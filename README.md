7c1c013c8468d4cd3bc9f0015ae617154faa7563
robinbood <mercedesfarts@gmail.com>
2025-05-16 08:06:39 +0500

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-it toook me 20s to write this and another 20s to push to github..<b>Learning recursion</b>
+it toook me 20s to write this and another 20s to push to github..<b>Learning recursion</b><hr />
+<em>And i was practising different stufff </em>
--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,54 @@ const App = () => {
   };
 };
 
+export const App2 = () => {
+  const pow = (x: number, n: number): number => {
+    return n === 1 ? x : x * pow(x, n - 1);
+  };
+};
+
+export class App3 {
+  name: number;
+  constructor(name: number) {
+    this.name = name;
+  }
+  method1(speech: string) {
+    alert(speech);
+  }
+}
+
+export const Vibe = () => {
+  const text = new TextEncoder();
+  const Uint8Array = text.encode("Hello");
+  alert(Uint8Array);
+};
+
+export const CSV = (File: File) => {
+  const  blob = new Blob([File], { type: "text/csv" });
+  const  reader = new FileReader();
+  reader.readAsText(blob);
+  const result = reader.result;
+  if (typeof result === "string") {
+    const arranged = result.split("\n")
+    const headers = arranged[0].split(",")
+    const rows = arranged.slice(1).map(row => {
+      const values = row.split(",").map(value => value.trim());
+      return headers.reduce((obj: Record<string, string>, header, index) => {
+        obj[header] = values[index] || '';
+        return obj;
+      }, {});
+    });
+    return rows;
+  } else {
+    return (
+      <div>
+        <h1>Error reading file</h1>
+      </div>
+    );
+  }
+};
 export default App;
 /* okay recursion works 
 tho i'm not sure why */
+
+// You actually never need recursion,it's quite memory-consuming and your event loop is going to take a nap
